62f270b9c68e5ac8f82d94d9735ac9c087294731
Rafael Bergo <rafael.desouzabergo@maine.edu>
2020-01-05 16:31:12 -0500

    Updates BorderLayoutEx2 with my idea of code organization.

--- SwingTutorial/src/swinglayoutmanagement/BorderLayoutEx2.java
+++ SwingTutorial/src/swinglayoutmanagement/BorderLayoutEx2.java
@@ -3,6 +3,7 @@ package swinglayoutmanagement;
 import java.awt.BorderLayout;
 import java.awt.EventQueue;
 import java.awt.Insets;
+import java.awt.event.ActionListener;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
@@ -36,61 +37,96 @@ public class BorderLayoutEx2 extends JFrame
      */
     private void initUI()
     {
-        var menuBar = new JMenuBar();
-        var fileMenu = new JMenu("File");
-        
-        menuBar.add(fileMenu);
-        setJMenuBar(menuBar);
-        
-        var toolBar = new JToolBar();
-        toolBar.setFloatable(false);
-        
-        var exitIcon = new ImageIcon("src/Resources/exit2.png");
-        var exitBtn = new JButton(exitIcon);
-        exitBtn.setBorder(new EmptyBorder(0, 0, 0, 0));
-        toolBar.add(exitBtn);
+        setJMenuBar(createMenuBar());
         
         // The toolbar is placed to the north of the layout.
-        add(toolBar, BorderLayout.NORTH);
-        
-        var vertical = new JToolBar(JToolBar.VERTICAL);
-        vertical.setFloatable(false);
-        vertical.setMargin(new Insets(10, 5, 5, 5));
-        
-        var driveIcon = new ImageIcon("src/Resources/drive.png");
-        var compIcon = new ImageIcon("src/Resources/computer.png");
-        var printIcon = new ImageIcon("src/Resources/printer.png");
-        
-        // To put some empty space around the button, an EmptyBorder is needed, adding some fixed space to the top and
-        // bottom of the button. When fixed spaces are added, the UI is not portable. A 3px space may look OK on a
-        // 1280x720 screen, but it is inappropriate on a 1920x1200 screen.
-        var driveBtn = new JButton(driveIcon);
-        driveBtn.setBorder(new EmptyBorder(3, 0, 3, 0));
-        
-        var compBtn = new JButton(compIcon);
-        compBtn.setBorder(new EmptyBorder(3, 0, 3, 0));
-        
-        var printBtn = new JButton(printIcon);
-        printBtn.setBorder(new EmptyBorder(3, 0, 3, 0));
-        
-        vertical.add(driveBtn);
-        vertical.add(compBtn);
-        vertical.add(printBtn);
+        add(createTopToolBar(), BorderLayout.NORTH);
         
         // The vertical toolbar is added to the west.
-        add(vertical, BorderLayout.WEST);
+        add(createLeftToolBar(), BorderLayout.WEST);
         
         // The text area is placed into the center.
         add(new JTextArea(), BorderLayout.CENTER);
         
         // The statusbar goes to the south area.
-        var statusbar = new JLabel(" Statusbar");
-        add(statusbar, BorderLayout.SOUTH);
+        add(new JLabel(" Statusbar"), BorderLayout.SOUTH);
         
         setSize(400, 350);
         setTitle("BorderLayout");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setLocationRelativeTo(null);
+    }
+    
+    /**
+     * Create the menubar.
+     */
+    private JMenuBar createMenuBar()
+    {
+        JMenuBar menuBar = new JMenuBar();
+        JMenu fileMenu = new JMenu("File");
+        menuBar.add(fileMenu);
+        return menuBar;
+    }
+    
+    /**
+     * Create the top toolbar.
+     */
+    private JToolBar createTopToolBar()
+    {
+        JToolBar toolBar = new JToolBar();
+        toolBar.setFloatable(false);
+        toolBar.add(createTopToolBarButton(new ImageIcon("src/Resources/exit2.png"), (e) -> System.exit(0)));
+        return toolBar;
+    }
+    
+    /**
+     * Create the left toolbar.
+     */
+    private JToolBar createLeftToolBar()
+    {
+        JToolBar toolBar = new JToolBar();
+        toolBar.setOrientation(JToolBar.VERTICAL);
+        toolBar.setFloatable(false);
+        toolBar.setMargin(new Insets(10, 5, 5, 5));
+        toolBar.add(createLeftToolBarButton(new ImageIcon("src/Resources/drive.png"), null));
+        toolBar.add(createLeftToolBarButton(new ImageIcon("src/Resources/computer.png"), null));
+        toolBar.add(createLeftToolBarButton(new ImageIcon("src/Resources/printer.png"), null));
+        return toolBar;
+    }
+    
+    /**
+     * Create a toolbar button.
+     */
+    private JButton createToolBarButton(ImageIcon icon, ActionListener listener)
+    {
+        JButton btn = new JButton(icon);
+        if (listener != null)
+            btn.addActionListener(listener);
+        return btn;
+    }
+    
+    /**
+     * Create a button for the top toolbar.
+     */
+    private JButton createTopToolBarButton(ImageIcon icon, ActionListener listener)
+    {
+        JButton btn = createToolBarButton(icon, listener);
+        btn.setBorder(new EmptyBorder(0, 0, 0, 0));
+        return btn;
+    }
+    
+    /**
+     * Create a button for the left toolbar.
+     * 
+     * To put some empty space around the button, an EmptyBorder is needed, adding some fixed space to the top and
+     * bottom of the button. When fixed spaces are added, the UI is not portable. A 3px space may look OK on a 1280x720
+     * screen, but it is inappropriate on a 1920x1200 screen.
+     */
+    private JButton createLeftToolBarButton(ImageIcon icon, ActionListener listener)
+    {
+        JButton btn = createToolBarButton(icon, listener);
+        btn.setBorder(new EmptyBorder(3, 0, 3, 0));
+        return btn;
     }
     
     // Driver
